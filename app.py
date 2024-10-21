053645bd2e8ae9c7525d456762da770eb2f9e234
Abhijeet Alande <abhijeetalande12@gmail.com>
2024-10-21 22:30:13 +0530

    final correction

--- app.py
+++ app.py
@@ -17,22 +17,20 @@ with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 st.title('Customer Churn Prediction')
-
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
-balance = st.number_input('Balance')
-credit_score = st.number_input('Credit Score')
-estimated_salary = st.number_input('Estimated Salary')
+balance = st.number_input('Balance', min_value=0.0, format='%f')  # Ensure it accepts float inputs
+credit_score = st.number_input('Credit Score', min_value=300, max_value=850, format='%d')
+estimated_salary = st.number_input('Estimated Salary', min_value=0.0, format='%f')
 tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
 
-# Prepare the input data
 input_data = pd.DataFrame({
     'CreditScore': [credit_score],
-    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Gender': [label_encoder_gender.transform([gender])[0]],  # Transform gender
     'Age': [age],
     'Tenure': [tenure],
     'Balance': [balance],
@@ -43,14 +41,13 @@ input_data = pd.DataFrame({
 })
 
 geo_encoded = onehot_encoder_geo.transform([[geography]])
-geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names(['Geography']))
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
 input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
 
 input_data_scaled = scaler.transform(input_data)
-
 prediction = model.predict(input_data_scaled)
-prediction_proba = prediction[0][0]
+prediction_proba = prediction[0][0] 
 
 st.write(f'Churn Probability: {prediction_proba:.2f}')
 
